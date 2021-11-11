5cd20a2fc8c2cf89c5935dd0790bd1f9b7d1cf57
SonyShrestha <sony.sth8@gmail.com>
2021-11-11 20:17:27 +0545

    Update GenerateRowsNTimesBasedOnColumnValue.sql

--- Interview_Questions/GenerateRowsNTimesBasedOnColumnValue.sql
+++ Interview_Questions/GenerateRowsNTimesBasedOnColumnValue.sql
@@ -22,3 +22,20 @@ where a.counter<b.quantity
 -- having count(a.item)=b.quantity
 )
 select name,item from cte;
+
+
+
+
+-- Generate rows N times based on column value
+set @cnt:=0;
+with recursive cte as (
+select name,last_name,1 as item,@cnt+1 as counter from item -- where name='Sony'
+union all
+select a.name,a.last_name,1 as item, counter+1 as counter from cte a
+inner join khwopa.item b
+on a.name=b.name
+and a.last_name=b.last_name
+where a.counter<b.quantity
+-- having count(a.item)=b.quantity
+)
+select name,last_name,item from cte;
